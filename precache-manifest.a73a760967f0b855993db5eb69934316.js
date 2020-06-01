af5c488e269f44684adad5aa6ddb72132a510841
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-06-01 17:11:54 -0300

    Updates

---
+++ precache-manifest.a73a760967f0b855993db5eb69934316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5b7e28c2a6d776f0d6ea70a9ce7b64",
+    "url": "/react-banco-inter/index.html"
+  },
+  {
+    "revision": "b409111760347e112a25",
+    "url": "/react-banco-inter/static/css/main.7f61fdae.chunk.css"
+  },
+  {
+    "revision": "5cbea667a9f3c1153942",
+    "url": "/react-banco-inter/static/js/2.4f94918d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-banco-inter/static/js/2.4f94918d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b409111760347e112a25",
+    "url": "/react-banco-inter/static/js/main.3237bfb8.chunk.js"
+  },
+  {
+    "revision": "1d7a7d2dbf8f453cf8c7",
+    "url": "/react-banco-inter/static/js/runtime-main.e5060d51.js"
+  },
+  {
+    "revision": "5265ff5722d9b033909a7a639285e38b",
+    "url": "/react-banco-inter/static/media/babi-mascara-covid.5265ff57.png"
+  }
+]);
